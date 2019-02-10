13a75c5e1f746378fe8b55e704e4e5157037b767
Matt Pritchard <t3hsauce@skynet>
2019-02-10 14:01:16 -0500

    adds exercises 3-4

---
+++ week_01/exercise_03.ml
@@ -0,0 +1,13 @@
+(* SIMPLE FUNCTIONS OVER INTEGERS  (20/20 points)
+ * Let's define two functions working with integers:
+ *
+ * multiple_of that takes two integer parameters, n and d, and determines whether n is a multiple of d. The function must return a boolean value. This function can be written without recursion. Look at the operators defined on integers in sequence 1.
+ * integer_square_root that calculates the integer square root of a positive integer n, that is the largest integer r such that r * r <= n. Hint: you may use floating point arithmetic, but don't forget that you have to convert explicitely between float and int. *)
+
+let multiple_of n d =
+  if n mod d = 0 then true else false;;
+
+let integer_square_root n =
+  let x = float_of_int n in
+  let y = floor (sqrt x) in
+  int_of_float y;;
